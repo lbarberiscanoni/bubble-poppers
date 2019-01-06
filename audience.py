b1566da39a8aa4ce41fd752482a41f5af6b6f19c
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-01-06 18:29:18 -0500

    added the agents

--- audience.py
+++ audience.py
@@ -10,45 +10,31 @@ import numpy as np
 
 class Audience:
 
-	def __init__(self, population_size, content):
-		self.graph = np.full((population_size, len(content)), 0)
+	def __init__(self, population_size):
+
+		self.content = {}
+
+		distribution = range(10)
+
+		index = 0
+		for x in distribution:
+			for y in distribution:
+				for z in distribution:
+					for w in distribution:
+						item = [x, y, z, w]
+						self.content[index] = item
+						index += 1
+
+		self.graph = np.full((population_size, len(self.content)), 0)
 		self.user_base = {}
-		self.item_dimensions = len(content[content.keys()[0]])
+		self.item_dimensions = len(self.content[self.content.keys()[0]])
 
 		for i in range(population_size):
 			local_user = User(i, self.item_dimensions)
 			self.user_base[i] = local_user
 
-		self.content = content
+	def recommendation(self, user_id, item_id):
+		reward = self.user_base[user_id].evaluate(self.content[item_id])
+		self.graph[user_id][item_id] = 1
 
-distribution = range(10)
-
-content = {}
-
-index = 100
-for x in tqdm(distribution):
-	for y in distribution:
-		for z in distribution:
-			for w in distribution:
-				item = [x, y, z, w]
-				content[index] = item
-				index += 1
-
-G = Audience(20, content)
-
-agent = PPOAgent(
-    states={"type":'float', "shape": G.graph.shape },
-    actions=dict(type="int", num_actions=3),
-    network=[
-	    dict(type='flatten'),
-	    dict(type="dense", size=32),
-    ],
-)
-
-print("agent ready")
-
-action = agent.act(G.graph)
-print(action)
-
-
-
+		return reward
